ad58db217b6b94297ae685d0493db402c4e048d2
Toze Cerdeira <tozecerdeira@hotmail.com>
2024-03-15 16:34:02 +0000

    Add project files.

---
+++ Chat Bass.net Teste/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Chat_Bass.net_Teste
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBarVolume = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarVolume)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(30, 117);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(161, 117);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Play";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.buttonPlay_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(289, 117);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(93, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Play 3 OK";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(19, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(346, 29);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Tentativas de uso BASS.DLL";
+            // 
+            // trackBarVolume
+            // 
+            this.trackBarVolume.Location = new System.Drawing.Point(405, 16);
+            this.trackBarVolume.Maximum = 100;
+            this.trackBarVolume.Name = "trackBarVolume";
+            this.trackBarVolume.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBarVolume.Size = new System.Drawing.Size(45, 104);
+            this.trackBarVolume.TabIndex = 4;
+            this.trackBarVolume.Value = 50;
+            this.trackBarVolume.Scroll += new System.EventHandler(this.trackBarVolume_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(405, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "50";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(491, 200);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trackBarVolume);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarVolume)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBarVolume;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Chat Bass.net Teste/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Un4seen.Bass;
+using Un4seen.Bass.AddOn.Fx;
+using Un4seen.Bass.Misc;
+
+
+
+namespace Chat_Bass.net_Teste
+{
+    public partial class Form1 : Form
+    {
+        double volume = 0.01;
+        public Form1()
+        {
+            InitializeComponent();
+            //BassNet.Registration(tozecerdeira@proton.me, 2X1824322253723);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Initialize BASS.NET
+            
+            //if (!Bass.BASS_Init(-1, 44100, BASSInit.BASS_DEVICE_DEFAULT, IntPtr.Zero))
+            //{
+            //    MessageBox.Show("BASS_Init error: " + Bass.BASS_ErrorGetCode());
+            //    return;
+            //}
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Audio Files|*.mp3;*.wav;*.ogg";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = openFileDialog.FileName;
+
+                // Load audio file
+
+                var _stream = Bass.BASS_StreamCreateFile(fileName, 0, 0, BASSFlag.BASS_DEFAULT);
+
+                if (_stream == 0)
+                {
+                    MessageBox.Show("BASS_StreamCreateFile error: " + Bass.BASS_ErrorGetCode());
+                    return;
+                }
+
+                // Apply effect (reverb)
+                
+                
+
+                //BASS_BFX_REVERB;
+                //BASS_BFX_REVERB(1, 5000);
+
+                //DSP_Reverb reverb = new DSP_Reverb(_stream);
+                
+                //reverb.FX_Bypass = false; // Enable the effect
+                //reverb.RoomSize = 0.5f;   // Set room size (adjust as needed)
+                //reverb.Damp = 0.5f;       // Set damping (adjust as needed)
+                //reverb.Wet = 0.5f;        // Set wet level (adjust as needed)
+                //reverb.Dry = 0.5f;        // Set dry level (adjust as needed)
+                
+                //// Add the effect to the stream
+                //reverb.AddToStream(_stream, BASSFXChan.BASS_BFX_CHANALL);
+                
+                // Play the modified audio
+                Bass.BASS_ChannelPlay(_stream, false);
+
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            //label2.Text = volume.ToString();
+            Bass.BASS_Init(-1,44100,BASSInit.BASS_DEVICE_DEFAULT, IntPtr.Zero);
+            // Codigo para funcionar o play
+            int stream = Bass.BASS_StreamCreateFile(@"c:\mp3\forest.mp3", 0, 0, BASSFlag.BASS_DEFAULT);
+            // Depois de tantas tenatativas descobri !! é preciso definir o volume do player !
+            Bass.BASS_ChannelSetAttribute(stream, BASSAttribute.BASS_ATTRIB_VOL, ((float)volume));
+            Bass.BASS_ChannelPlay(stream, false);
+            
+            //Bass.BASS_StreamFree(stream);
+        }
+
+        private void buttonPlay_Click(object sender, EventArgs e)
+        {
+            
+            string mp3file = @"c:\mp3\amarteassim.mp3";
+
+            int stream = Bass.BASS_StreamCreateFile(mp3file, 0, 0, BASSFlag.BASS_DEFAULT);
+            //int stream = Bass.BASS_StreamCreateFile(mp3file, 0, 0, BASSFlag.BASS_SAMPLE_FLOAT);
+
+            //BASSFlag.BASS_SAMPLE_FLOAT | BASSFlag.BASS_STREAM_PRESCAN);
+
+            if (stream != 0 && Bass.BASS_ChannelPlay(stream, false))
+            {
+                
+                //Bass.BASS_ChannelGetAttribute(stream, BASSAttribute.BASS_ATTRIB_REVERSE_DIR, volume);
+                Bass.BASS_ChannelPlay(stream, false);
+            }
+            else
+            {
+                Console.WriteLine("Error={0}", Bass.BASS_ErrorGetCode());
+            }
+            Bass.BASS_StreamFree(stream);
+        }
+
+        private void trackBarVolume_Scroll(object sender, EventArgs e)
+        {
+            volume = trackBarVolume.Value*0.01;
+            label2.Text = trackBarVolume.Value.ToString();
+        }
+    }
+}
